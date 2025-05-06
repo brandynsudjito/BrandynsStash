fa9307d90436c3b8e6678a4ef7dde3d237b39322
Brandyn Sudjito <brandyn176@gmail.com>
2025-05-06 08:21:29 -0400

    feat: add home page

---
+++ brandyns-stash/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+
+const Home = () => {
+
+    return (
+        <>
+            <div className="home">
+                <h1>Welcome to the Home Page</h1>
+                <p>This is a simple home page component.</p>
+            </div>
+        </>
+    );
+};
+
+export default Home;
