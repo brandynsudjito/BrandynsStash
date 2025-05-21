122dfac5a509b2abe75180e5b0f4baef271b97e9
Brandyn Sudjito <brandyn176@gmail.com>
2025-05-21 16:05:00 -0400

    feat: add functionality for searching for single series/series with multiple names

--- brandyns-stash/src/components/list.jsx
+++ brandyns-stash/src/components/list.jsx
@@ -8,15 +8,24 @@ function List(props) {
         if (props.input === '') {
             return el;
         }
+        const inputLower = props.input.toLowerCase();
+            // Convert series to array if it's not already an array
+            const seriesArray = Array.isArray(el.series) 
+            ? el.series 
+            : [el.series];
+
         //return the item which contains the user input
-        else {
-            return el.text.toLowerCase().includes(props.input)
-        }
+        return (
+            el.name.toLowerCase().includes(inputLower) || 
+            seriesArray.some(series => 
+                series.toLowerCase().includes(inputLower)
+            )
+        );
     })
     return (
         <ul>
             {filteredData.map((item) => (
-                <li key={item.id}>{item.text}</li>
+                <li key={item.id}>{item.name}</li>
             ))}
         </ul>
     )
