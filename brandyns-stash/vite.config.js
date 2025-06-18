126f684975391673ce95765e5a878f0f945b5e0e
Brandyn Sudjito <brandyn176@gmail.com>
2025-06-17 21:12:19 -0400

    fix

--- brandyns-stash/vite.config.js
+++ brandyns-stash/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import jsconfigPaths from 'vite-jsconfig-paths'
+import * as path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
